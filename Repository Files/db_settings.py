992a14d9b9bd914cd69e7e6fbed9c9b61ceb9abe
Lawrence P <188268285+Shumpman0404@users.noreply.github.com>
2024-12-09 10:26:45 -0500

    Update db_settings.py

--- Repository Files/db_settings.py
+++ Repository Files/db_settings.py
@@ -1,6 +1,6 @@
 # db_settings.py
 
-DB_HOST = 'AI-Safety_postgres_db'
+DB_HOST = 'AI-Safety_postgres_db_V2'
 DB_PORT = '5432'
 DB_NAME = 'AI-Safety-Through-Debate'
 DB_USER = 'AI-Safety-Through-Debate'
